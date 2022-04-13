753fb91065e65ab0c62b30a51513ffed25040db1
pubudu12 <pubudu.senadheera@yahoo.com>
2022-04-13 10:44:58 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Innovatives Pvt Ltd</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/fontawesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.css"/>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Logo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse navbar-links" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container-fluid">
+        <div class="row slider-main">
+            <!-- <div class=""> -->
+                <div class="slides">
+                    <img src="assets/images/s1.jpg" alt="">
+                    <div class="slider-text">
+                        <h1 class="main-heading">Experience the Creativity</h1>
+                        <h3>A Place where imagination meets ambition.</h3>
+                    </div>
+                    <div class="slider-overlay"></div>
+                </div>
+                <div class="slides">
+                    <img src="assets/images/s2.jpg" alt="">
+                    <div class="slider-text">
+                        <h1 class="main-heading">Experience the Creativity</h1>
+                        <h3>A Place where imagination meets ambition.</h3>
+                    </div>
+                    <div class="slider-overlay"></div>
+                </div>
+                <div class="slides">
+                    <img src="assets/images/s3.jpg" alt="">
+                    <div class="slider-text">
+                        <h1 class="main-heading">Experience the Creativity</h1>
+                        <h3>A Place where imagination meets ambition.</h3>
+                    </div>
+                    <div class="slider-overlay"></div>
+                </div>
+            <!-- </div> -->
+        </div>
+    </div>
+    <!-- end of slider -->
+
+    <!-- start of second section -->
+    <div class="container section-2">
+        <div class="row">
+            <div class="col-md-6 col-sm-12 col-lg-6">
+                <div class="section-2__images" >
+                    <!-- <div class="section-2__1st-layer-image"> -->
+                        <img class="section-2__image1" src="assets/images/sec2_1.jpg" alt="img">
+                            <!-- <div class="section-2__2nd-layer-image"> -->
+                        <img class="section-2__image2" src="assets/images/s4.jpg" alt="img">
+                            <!-- </div> -->
+                    <!-- </div> -->
+                </div>
+            </div>
+            <div class="form-group col-md-6 col-sm-12 col-lg-6 section-2__text">
+                <h2 class="section-2__h2-style">Go Innovative</h2>
+                <div class="section-2__paragraph">
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate repellendus quas nesciunt magnam perferendis animi consequatur ea fugit nisi voluptatibus quia, error corrupti id incidunt soluta. Consectetur pariatur quibusdam rem?
+                    </p>
+                </div>
+                <a href="#" class="section-2__btn-read-more">read more</a>
+            </div>
+        </div>
+    </div>
+    <!-- end of second section -->
+
+    <!-- start of section three -->
+    <div class="container-fluid section-3">
+        <div class="section-3__topic-text">
+            <div class="section-3__small-text">
+                Easy way to build websites
+            </div>
+            <div class="section-3__black-text">
+                New stunning projects for our amazing clients
+            </div>
+        </div>
+        <div>
+            <div class="section-3__card-section row">
+                <div class="col-md-3 col-sm-12 col-lg-3 section-3__white-section">
+                    <h3>01</h3>
+                    <div class="section-3__t1">
+                        Discover talent
+                    </div>
+                    <div class="section-3__t2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </div>
+                    <div class="section-3__card-view-more">
+                        <a href="">View more</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-12 col-lg-3 section-3__white-section">
+                    <h3>02</h3>
+                    <div class="section-3__t1">
+                        Expert analysis
+                    </div>
+                    <div class="section-3__t2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </div>
+                    <div class="section-3__card-view-more">
+                        <a href="">View more</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-12 col-lg-3 section-3__white-section">
+                    <h3>03</h3>
+                    <div class="section-3__t1">
+                        Explore work
+                    </div>
+                    <div class="section-3__t2">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </div>
+                    <div class="section-3__card-view-more">
+                        <a href="">View more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end of section three -->
+
+    <!-- Start section 4 -->
+    <div class="container-fluid section-4">
+        <div class="section-4__topic-text1">
+            <div class="section-4__text1">
+                Easy way to build websites
+            </div>
+            <div class="section-4__text2">
+                New stunning projects for our amazing clients
+            </div>
+        </div>
+        
+        <div class="row section-4__masonary-part"> 
+            <!-- <div class="col-md-3 section-4__masonary-part__main">
+                <div>
+                    <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/1.jpg" style="width:100%">
+                    <div class="section-4__masonary-part__main__white-trns">
+                        <h4>Main topic here</h4>
+                        <small>Branding and Identity</small>
+                    </div>
+                </div>
+                <div>
+                    <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/2.jpg" style="width:100%">
+                    <div class="section-4__masonary-part__main__white-trns1">
+                        <h4>Main topic here</h4>
+                        <small>Branding and Identity</small>
+                    </div>
+                </div>
+            </div> -->
+            
+            <div class="col-md-3 section-4__masonary-part__main">
+                <!-- <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/9.jpg" style="width:100%"> -->
+                <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/4.jpg" style="width:100%">
+            </div> 
+            
+            <div class="col-md-3 section-4__masonary-part__main">
+                <!-- <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/1.jpg" style="width:100%"> -->
+                <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/3.jpg" style="width:100%">
+            </div>
+            
+            <div class="col-md-3 section-4__masonary-part__main">
+                <!-- <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/5.jpg" style="width:100%"> -->
+                <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/7.jpg" style="width:100%">
+            </div>
+            <div class="col-md-3 section-4__masonary-part__main">
+                <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/5.jpg" style="width:100%">
+                <!-- <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/7.jpg" style="width:100%"> -->
+            </div>
+            <div class="col-md-3 section-4__masonary-part__main">
+                <!-- <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/5.jpg" style="width:100%"> -->
+                <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/9.jpg" style="width:100%">
+            </div>
+            <div class="col-md-3 section-4__masonary-part__main">
+                <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/1.jpg" style="width:100%">
+                <!-- <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/7.jpg" style="width:100%"> -->
+            </div>
+            <div class="col-md-3 section-4__masonary-part__main">
+                <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/2.jpg" style="width:100%">
+                <!-- <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/7.jpg" style="width:100%"> -->
+            </div>
+            <div class="col-md-3 section-4__masonary-part__main">
+                <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/6.jpg" style="width:100%">
+                <!-- <img class="section-4__masonary-part__main__img mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/7.jpg" style="width:100%"> -->
+            </div>
+        </div>
+
+        <div class="masonry">
+            <div class="col--2x">
+                <img class=" mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/6.jpg" style="width:100%; height: auto;">
+            </div>
+            <div class="col--3x">
+                <img class=" mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/7.jpg" style="width:100%; height: auto;">
+            </div>
+            <div class="col--2x">
+                <img class=" mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/6.jpg" style="width:100%; height: auto;">
+            </div>
+            <div class="col--3x">
+                <img class=" mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/6.jpg" style="width:100%; height: auto;">
+            </div>
+            <div class="col--2x">
+                <img class=" mb-3" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/6.jpg" style="width:100%; height: auto;">
+            </div>
+        </div>
+    </div>
+    <!-- End section 4 -->
+
+
+    <!-- footer -->
+    
+    <!-- end footer -->
+
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/fontawesome.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script type="text/javascript">
+    // console.log($)
+        $(document).ready(function(){
+            $('.slider-main').slick({
+                // centerMode:true,
+                // dots: true,
+                // fade: true,
+                slidesToShow:1,
+                autoplay:true,
+                autoplaySpeed:3000
+            });
+        });
+  </script>
+
+    <script>
+        AOS.init();
+    </script>
+</body>
+</html>
